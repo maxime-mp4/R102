14fcbd6e061511ebf53bf532c093d37a36755eea
Maxime <maxime.arbadji@icloud.com>
2024-12-30 00:49:53 +0100

    fix: add page

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,79 +1,55 @@
-
-
-
-
-
-
-
-
 const availableThemes = ["default", "mono"];
 const stylesheet = document.styleSheets[0];
+const themeSwitcher = document.getElementById("theme-switcher");
 
 
-console.log(localStorage.getItem("theme"))
-
-if(!availableThemes.includes(localStorage.getItem("theme")))
-{
+if(!availableThemes.includes(localStorage.getItem("theme"))){
     localStorage.clear()
 }
 
 function removeRule(url){
-    const stylesheet = document.styleSheets[0]; // Sélectionne la première feuille de style
+    const stylesheet = document.styleSheets[0];
 
-// Parcourt les règles de la feuille de style
-for (let i = 0; i < stylesheet.cssRules.length; i++) {
-  const rule = stylesheet.cssRules[i];
-  
-  // Vérifie si c'est une règle @import et si l'URL correspond à './dynamic.css'
-  if (rule instanceof CSSImportRule && rule.href.includes(url)) {
-    stylesheet.deleteRule(i); // Supprime la règle à l'index i
-    break; // Arrête après suppression
-  }
-}
-
+    for (let i = 0; i < stylesheet.cssRules.length; i++) {
+        if (stylesheet.cssRules[i] instanceof CSSImportRule && stylesheet.cssRules[i].href.includes(url)) {
+        break;
+        }
+    }
 }
 
 function checkTheme(){
     if(!localStorage.getItem("theme")){
         localStorage.setItem("theme", "default")
-        console.log("Aucun theme n'a été défini. Le thème sera donc par défaut")
-    } else {
-        console.log(`${localStorage.getItem("theme")} actif.`)
-    }    
+    }   
 }
 
 function loadTheme(){
     switch(localStorage.getItem("theme")){
         case "default":
-            console.log("Default loading...")
             document.styleSheets[0].insertRule("@import url('./dynamic.css')");
-            removeRule("./mono.css");
         break;
     
         case "mono":
-            console.log("Mono loading...")
             document.styleSheets[0].insertRule("@import url('./mono.css')");
-            removeRule("./dynamic.css");
         break;
     }
 }
 
-const themeSwitcher = document.getElementById("theme-switcher");
-
 function switchTheme () {
-    console.log(localStorage.getItem("theme") === "default" ? true : false)
-
     localStorage.getItem("theme") === "default" ? localStorage.setItem("theme", "mono") : localStorage.setItem("theme", "default")
     loadTheme();
     checkTheme()
 }
 
 
-document.addEventListener("DOMContentLoaded", (e) => {
-    loadTheme()
-    checkTheme()
+window.addEventListener("load", (e) => {
+    loadTheme();
+    setTimeout(() => {
+        document.body.style.display = "unset"
+    }, 250);
 })
 
 themeSwitcher.addEventListener("click", (e) => {
     switchTheme();
+    location.reload()
 });
